c7cf794d75a1b3d433648e435d87264fd1d466b9
Enow Mbi <benowmbi@yahoo.com>
2021-11-09 16:30:09 +0100

    Add articles component

---
+++ src/components/articles.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Article from '../components/article';
+
+
+const Articles = (props) =>{
+    return(
+        <ul id="listOfArticles" className="media-list">
+        {
+            props.articles.map(article =>(
+
+                <li key ={`${article.id}-${article.title}`}>
+                <Article 
+                id = {article.id}
+                name = {article.name}
+                author = {article.author}
+                title = {article.title}
+                description ={article.description}
+                url = {article.url}
+                urlToImage = {article.urlToImage}
+                publishedAt = {article.publishedAt}
+                content = {article.content}
+                />
+                </li>
+
+            )
+            )
+        }
+        </ul>
+
+    )
+}
+
+export default Articles
