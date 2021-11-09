39c0b559686ea39bb8d65d488863a03efbf00544
Enow Mbi <benowmbi@yahoo.com>
2021-11-09 16:30:37 +0100

    Add main component

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Articles from '../components/articles'
+
+const Main = (props) =>{
+    return(
+        <React.Fragment>
+        <Articles articles = {props.articles} />
+        </React.Fragment>
+    )
+}
+
+export default Main
