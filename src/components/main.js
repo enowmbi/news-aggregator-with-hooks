8be14e75bcc7be99cbaca04e89d055cb8c484993
Enow Mbi <benowmbi@yahoo.com>
2023-06-19 11:59:31 +0100

    Use ES7 syntax for components

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,8 @@
-import React from 'react'
 import Articles from '../components/articles'
 
 const Main = (props) =>{
     return(
-        <React.Fragment>
         <Articles articles = {props.articles} />
-        </React.Fragment>
     )
 }
 
